bad43013c9a37fb6ac26b85c0b60e77d809a4472
Shonennn <Shonennn@users.noreply.github.com>
2022-09-22 23:50:57 -0400

    test

--- classExercice_1.py
+++ classExercice_1.py
@@ -1,6 +1,13 @@
 str1 = int (input("Input first value："));
 str2 = int (input("Input second value："));
-print("Operation + :", (str1 + str2));
-print("Operation * :", (str1 * str2));
-print("Operation / :", (str1 * str2));
-print("Operation ** :", (str1 ** str2));
+print("A: Operation + :", (str1 + str2));
+print("B: Operation * :", (str1 * str2));
+print("C: Operation / :", (str1 / str2));
+print("D: Operation ** :", (str1 ** str2));
+
+str3 = int(input("Input third value："));
+print("modification of a =",(str1+str2+str3));
+print("modification of b =",(str1*str2*str3));
+print("modification of c =",(str1/str2/str3));
+print("modification of d =",(str1**str2**str3));
+
